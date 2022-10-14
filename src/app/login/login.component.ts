718ec5543ec76382f05ad5fca06ea175fec63e5f
Alejandro Ferrera Hernandez <aferrerah1421@gmail.com>
2022-10-14 03:08:48 -0300

    started the project for real

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  email = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+  ]);
+  password = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  getErrorEmailMessage() {
+    if (this.email.hasError('required')) {
+      return 'Debe introducir un correo';
+    }
 
-  ngOnInit(): void {
+    return this.email.hasError('pattern') ? 'Correo no válido' : '';
   }
 
+  getErrorPasswordMessage() {
+    if (this.password.hasError('required')) {
+      return 'Debe introducir una contraseña';
+    }
+    return '';
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
