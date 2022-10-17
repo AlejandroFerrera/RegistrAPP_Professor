875a2b8f32eb75e1f786fa927a1413366adcc5ca
Alejandro Ferrera Hernandez <aferrerah1421@gmail.com>
2022-10-17 00:40:49 -0300

    pre layout func

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,9 @@ import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SectionsComponent } from './sections/sections.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { SectionsComponent } from './sections/sections.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, SectionsComponent],
